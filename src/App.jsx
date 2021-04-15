654c0ca5974060310879f73a9f4883ffa44e50f6
valerdev <valer.val7@gmail.com>
2021-04-15 20:00:26 +0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                My App Component
+            </div>
+        );
+    }
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+console.log('IS DEV:', isDev);
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: filename('js'),
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|.jsx$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|j?g|svg|gif)?$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: filename('html')
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+
+        })
+    ]
+};
